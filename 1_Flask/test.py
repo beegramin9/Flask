774ac4bff6240966f3ac7f52e58e5c3515630590
wontae <beegramin9@gmail.com>
2020-12-14 09:40:47 +0900

    날씨

--- 1_Flask/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-a = np.arange(1,10,1)
-print(a)
-
-b = pd.DataFrame()
-print(type(b))
--- 2_Bootstrap/app.py
+++ 2_Bootstrap/app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from my_util.weather import get_weather
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hello Flask :)'
+    menu = {'ho': 1, 'da': 0, 'ml': 0, 'se': 0,
+            'co': 0, 'cg': 0, 'cr': 0, 'st': 0, 'wc': 0}
+    return render_template('main.html', menu=menu, weather=get_weather())
+
+
+@app.route('/park')
+def park():
+    menu = {'ho': 0, 'da': 1, 'ml': 0, 'se': 1,
+            'co': 0, 'cg': 0, 'cr': 0, 'st': 0, 'wc': 0}
+    return render_template('main.html', menu=menu, weather=get_weather())
+
+
+@app.route('/stock', methods=['GET', 'POST'])
+def stock():
+    menu = {'ho': 0, 'da': 1, 'ml': 0, 'se': 1,
+            'co': 0, 'cg': 0, 'cr': 0, 'st': 0, 'wc': 0}
+    if request.method == 'GET':
+        return render_template('stock.html', menu=menu, weather=get_weather())
+    else:
+        return render_template('stock_res.html', menu=menu, weather=get_weather())
+
 
 if __name__ == '__main__':
-    # debug=True Option: nodemon과 같은 역할
     app.run(debug=True)
