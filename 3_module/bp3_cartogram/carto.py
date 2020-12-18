03215dada05663299cc52873d0c7a86eb690666c
wontae <beegramin9@gmail.com>
2020-12-18 11:19:56 +0900

    워드클라우드

--- 3_module/bp3_cartogram/carto.py
+++ 3_module/bp3_cartogram/carto.py
@@ -39,7 +39,7 @@ def get_weather_main():
 @carto_bp.route('/coffee', methods=['get', 'post'])
 def coffee():
     menu = {'ho': 0, 'da': 1, 'ml': 0, 'se': 0,
-            'co': 0, 'cg': 0, 'cr': 1, 'st': 0, 'wc': 0}
+            'co': 0, 'cg': 1, 'cr': 0, 'st': 0, 'wc': 0}
     if request.method == 'GET':
         options = ['커피지수', '이디야 매장수', '스타벅스 매장수', '커피빈 매장수', '빽다방 매장수']
         return render_template('cartogram/coffee.html', menu=menu, weather=get_weather_main(), options=options)
@@ -50,6 +50,7 @@ def coffee():
         f = request.files['csv']
         # secure_filename: 불특정 다수에게 입력을 받을 때 불완전요소를 막기 위한 장치
         # secure_filename(f.filename). 한글이름으로 된 파일을 어떻게 올릴 수 있을까?
+
         filename = os.path.join(current_app.root_path,
                                 'static\\upload\\') + f.filename
 
@@ -58,12 +59,76 @@ def coffee():
 
         current_app.logger.info(f'{filename} is saved.')
 
+        colormaps = {
+            '커피지수': 'Reds',
+            '스타벅스 매장수': 'Greens',
+            '커피빈 매장수': 'Oranges',
+            '이디야 매장수': 'Blues',
+            '빽다방 매장수': 'PuRd'
+        }
+
         coffee = pd.read_csv(filename, encoding='utf8')
 
         img_file = os.path.join(current_app.root_path, 'static/img/coffee.png')
 
-        drawKorea(item, coffee, 'Reds', img_file)
+        drawKorea(item, coffee, colormaps[item], img_file)
         mtime = int(os.stat(img_file).st_mtime)
 
-        return render_template('cartogram/coffee_res.html', menu=menu, weather=get_weather_main(), mtime=mtime)
-        # cartogram = pd.read_excel('./static/data/cartogram/전국 카토그램.xlsx', engine = 'openpyxl')
+        # top10
+        top10 = coffee[['ID', item]].sort_values(
+            by=item, ascending=False).head(10)
+        top10[item] = top10.apply(lambda r: round(r[item], 2), axis=1)
+
+        return render_template('cartogram/coffee_res.html', menu=menu, weather=get_weather_main(), mtime=mtime,
+                               item=item, top10=top10)
+
+
+@carto_bp.route('/population', methods=['get', 'post'])
+def population():
+    menu = {'ho': 0, 'da': 1, 'ml': 0, 'se': 0,
+            'co': 0, 'cg': 1, 'cr': 0, 'st': 0, 'wc': 0}
+    if request.method == 'GET':
+        options = ['인구수계', '소멸위기지역', '여성비', '2030여성비']
+        return render_template('cartogram/population.html', menu=menu, weather=get_weather_main(), options=options)
+    else:
+        # 셀렉트박스에서 선택한 항목 받아오기
+        item = request.form['item']
+        # csv파일 받아오기
+        f = request.files['csv']
+        # secure_filename: 불특정 다수에게 입력을 받을 때 불완전요소를 막기 위한 장치
+        # secure_filename(f.filename). 한글이름으로 된 파일을 어떻게 올릴 수 있을까?
+
+        filename = os.path.join(current_app.root_path,
+                                'static\\upload\\') + f.filename
+
+        f.save(filename)
+
+        colormaps = {
+            '인구수계': 'Blues',
+            '소멸위기지역': 'Reds',
+            '여성비': 'RdBu',
+            '2030여성비': 'RdBu'
+        }
+
+        population = pd.read_csv(filename, encoding='utf8')
+
+        img_file = os.path.join(current_app.root_path, 'static/img/coffee.png')
+
+        drawKoreaMinus(item, population, colormaps[item], img_file)
+        mtime = int(os.stat(img_file).st_mtime)
+
+        # top10
+        # 여성비가 작은 지역순으로 나옴
+        if item.find('여성비') >= 0:
+            top10 = population[['ID', item]].sort_values(
+                by=item, ascending=True).head(10)
+        else:
+            top10 = population[['ID', item]].sort_values(
+                by=item, ascending=False).head(10)
+
+        top10[item] = top10.apply(lambda r: round(r[item], 2), axis=1)
+        top10.reset_index(inplace=True)
+        del top10['index']
+
+        return render_template('cartogram/population_res.html', menu=menu, weather=get_weather_main(), mtime=mtime,
+                               item=item, top10=top10)
