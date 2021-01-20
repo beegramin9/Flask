486053abd42292c678de6b8334ea1c66190f7568
wontae <beegramin9@gmail.com>
2021-01-20 16:14:17 +0900

    유방암 이진 분류

--- 3_module/app.py
+++ 3_module/app.py
@@ -4,8 +4,9 @@ from flask import Flask, render_template, session, request
 from bp1_seoul.seoul import seoul_bp
 from bp2_covid.covid import covid_bp
 from bp3_cartogram.carto import carto_bp
+from bp4_wordcloud.wordcloud import word_bp
 from bp5_stock.stock import stock_bp
-from bp6_wordcloud.wordcloud import word_bp
+from bp5_classification.clsf import clsf_bp
 
 
 from datetime import datetime, timedelta
@@ -32,8 +33,9 @@ app.config['SESSION_COOKIE_PATH'] = '/'
 app.register_blueprint(seoul_bp, url_prefix='/seoul')
 app.register_blueprint(covid_bp, url_prefix='/covid')
 app.register_blueprint(carto_bp, url_prefix='/cartogram')
-app.register_blueprint(stock_bp, url_prefix='/stock')
 app.register_blueprint(word_bp, url_prefix='/wordcloud')
+app.register_blueprint(stock_bp, url_prefix='/stock')
+app.register_blueprint(clsf_bp, url_prefix='/classification')
 
 """ with open('./logging.json', 'r') as file:
     config = json.load(file)
