0801fb6a28c086b5c69e6736ec0db3c2da947cbd
wontae <beegramin9@gmail.com>
2021-01-25 16:19:51 +0900

    아이리스 모델

--- 3_module/bp6_classification/clsf.py
+++ 3_module/bp6_classification/clsf.py
@@ -34,11 +34,11 @@ def cancer():
         return render_template('classification/cancer.html', menu=menu, weather=get_weather())
     else:
         index = int(request.form['index'])
-        df = pd.read_csv('static/data/classification/cancer_test.csv')
+        df_test = pd.read_csv('static/data/classification/cancer_test.csv')
         scaler = MinMaxScaler()
-        scaled_test = scaler.fit_transform(df.iloc[:, :-1])
+        scaled_test = scaler.fit_transform(df_test.iloc[:, :-1])
         test_data = scaled_test[index, :].reshape(1, -1)
-        label = df.iloc[index, -1]
+        label = df_test.iloc[index, -1]
         lrc = joblib.load('static/model/cancer_lr.pkl')
         svc = joblib.load('static/model/cancer_sv.pkl')
         rfc = joblib.load('static/model/cancer_rf.pkl')
@@ -47,7 +47,7 @@ def cancer():
         pred_rf = rfc.predict(test_data)
         result = {'index': index, 'label': label,
                   'pred_lr': pred_lr[0], 'pred_sv': pred_sv[0], 'pred_rf': pred_rf[0]}
-        org = df.iloc[index, :-1].to_dict()
+        org = df_test.iloc[index, :-1].to_dict()
         return render_template('classification/cancer_res.html', menu=menu,
                                res=result, org=org, weather=get_weather())
 
@@ -61,13 +61,13 @@ def titanic():
         return render_template('classification/titanic.html', menu=menu, weather=get_weather())
     else:
         index = int(request.form['index'])
-        df_train = pd.read_csv('static/data/classification/titanic_train.csv')
+        df_test = pd.read_csv('static/data/classification/titanic_test.csv')
         scaler = joblib.load('static/model/titanic_scaler.pkl')
         # 스케일러를 잘못건드린듯
         # 여기서 문제생긴듯
-        test_data = df_train.iloc[index, 1:].values.reshape(1, -1)
+        test_data = df_test.iloc[index, 1:].values.reshape(1, -1)
         test_scaled = scaler.transform(test_data)
-        label = df_train.iloc[index, 0]
+        label = df_test.iloc[index, 0]
         lrc = joblib.load('static/model/titanic_lr.pkl')
         svc = joblib.load('static/model/titanic_sv.pkl')
         rfc = joblib.load('static/model/titanic_rf.pkl')
@@ -76,7 +76,7 @@ def titanic():
         pred_rf = rfc.predict(test_scaled)
         result = {'index': index, 'label': label,
                   'pred_lr': pred_lr[0], 'pred_sv': pred_sv[0], 'pred_rf': pred_rf[0]}
-        org = df_train.iloc[index, 1:].to_dict()
+        org = df_test.iloc[index, 1:].to_dict()
         return render_template('classification/titanic_res.html', menu=menu,
                                res=result, org=org, weather=get_weather())
 
@@ -90,12 +90,12 @@ def pima():
         return render_template('classification/pima.html', menu=menu, weather=get_weather())
     else:
         index = int(request.form['index'])
-        df_train = pd.read_csv('static/data/classification/pima_train.csv')
+        df_test = pd.read_csv('static/data/classification/pima_test.csv')
         # 여기 scaler는 이미 fit이 된 상태, transform만 하면 된다.
         scaler = joblib.load('static/model/pima_scaler.pkl')
-        test_data = df_train.iloc[index, :-1].values.reshape(1, -1)
+        test_data = df_test.iloc[index, :-1].values.reshape(1, -1)
         test_scaled = scaler.transform(test_data)
-        label = df_train.iloc[index, -1]
+        label = df_test.iloc[index, -1]
         lrc = joblib.load('static/model/pima_lr.pkl')
         svc = joblib.load('static/model/pima_sv.pkl')
         rfc = joblib.load('static/model/pima_rf.pkl')
@@ -105,6 +105,65 @@ def pima():
         result = {'index': index, 'label': label,
                   'pred_lr': pred_lr[0], 'pred_sv': pred_sv[0], 'pred_rf': pred_rf[0]}
 
-        org = df_train.iloc[index, :-1].to_dict()
+        org = df_test.iloc[index, :-1].to_dict()
         return render_template('classification/pima_res.html', menu=menu,
+                               res=result, org=org, weather=get_weather())
+
+
+@clsf_bp.route('/iris', methods=['GET', 'POST'])
+def iris():
+    menu = {'ho': 0, 'da': 0, 'ml': 10,
+            'se': 0, 'co': 0, 'cg': 0, 'cr': 0, 'wc': 0,
+            'cf': 1, 'ac': 0, 're': 0, 'cu': 0}
+    if request.method == 'GET':
+        return render_template('classification/iris.html', menu=menu, weather=get_weather())
+    else:
+        index = int(request.form['index'])
+        df_test = pd.read_csv('static/data/classification/iris_test.csv')
+        # 여기 scaler는 이미 fit이 된 상태, transform만 하면 된다.
+        scaler = joblib.load('static/model/iris_scaler.pkl')
+        test_data = df_test.iloc[index, :-1].values.reshape(1, -1)
+        test_scaled = scaler.transform(test_data)
+        label = df_test.iloc[index, -1]
+        lrc = joblib.load('static/model/iris_lr.pkl')
+        svc = joblib.load('static/model/iris_sv.pkl')
+        rfc = joblib.load('static/model/iris_rf.pkl')
+        pred_lr = lrc.predict(test_scaled)
+        pred_sv = svc.predict(test_scaled)
+        pred_rf = rfc.predict(test_scaled)
+        result = {'index': index, 'label': label,
+                  'pred_lr': pred_lr[0], 'pred_sv': pred_sv[0], 'pred_rf': pred_rf[0]}
+
+        org = df_test.iloc[index, :-1].to_dict()
+        return render_template('classification/iris_res.html', menu=menu,
+
+                               res=result, org=org, weather=get_weather())
+
+
+@clsf_bp.route('/wine', methods=['GET', 'POST'])
+def wine():
+    menu = {'ho': 0, 'da': 0, 'ml': 10,
+            'se': 0, 'co': 0, 'cg': 0, 'cr': 0, 'wc': 0,
+            'cf': 1, 'ac': 0, 're': 0, 'cu': 0}
+    if request.method == 'GET':
+        return render_template('classification/wine.html', menu=menu, weather=get_weather())
+    else:
+        index = int(request.form['index'])
+        df_test = pd.read_csv('static/data/classification/wine_test.csv')
+        # 여기 scaler는 이미 fit이 된 상태, transform만 하면 된다.
+        scaler = joblib.load('static/model/wine_scaler.pkl')
+        test_data = df_test.iloc[index, :-1].values.reshape(1, -1)
+        test_scaled = scaler.transform(test_data)
+        label = df_test.iloc[index, -1]
+        lrc = joblib.load('static/model/wine_lr.pkl')
+        svc = joblib.load('static/model/wine_sv.pkl')
+        rfc = joblib.load('static/model/wine_rf.pkl')
+        pred_lr = lrc.predict(test_scaled)
+        pred_sv = svc.predict(test_scaled)
+        pred_rf = rfc.predict(test_scaled)
+        result = {'index': index, 'label': label,
+                  'pred_lr': pred_lr[0], 'pred_sv': pred_sv[0], 'pred_rf': pred_rf[0]}
+
+        org = df_test.iloc[index, :-1].to_dict()
+        return render_template('classification/wine_res.html', menu=menu,
                                res=result, org=org, weather=get_weather())
